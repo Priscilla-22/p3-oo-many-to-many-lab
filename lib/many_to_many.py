52303e371891a02d1d963157d38edc656b077e9e
Priscilla-22 <priscillamw123@gmail.com>
2024-03-09 18:21:43 +0300

    feat:Many-to-many Object Relationships

--- lib/many_to_many.py
+++ lib/many_to_many.py
@@ -1,10 +1,63 @@
+from datetime import datetime
+
+
 class Author:
-    pass
+    def __init__(self, name):
+        self.name = name
+        self._contracts = []
+
+    def contracts(self):
+        return self._contracts
+
+    def books(self):
+        return [contract.book for contract in self._contracts]
+
+    def sign_contract(self, book, date, royalties):
+        if not isinstance(date, str):
+            raise TypeError("Date must be a string")
+        if not isinstance(royalties, int):
+            raise TypeError("Royalties must be an integer")
+
+        contract = Contract(self, book, date, royalties)
+        self._contracts.append(contract)
+        return contract
+
+    def total_royalties(self):
+        return sum(contract.royalties for contract in self._contracts)
 
 
 class Book:
-    pass
+    def __init__(self, title):
+        self.title = title
+        self._contracts = []
+
+    def contracts(self):
+        return self._contracts
+
+    def authors(self):
+        return [contract.author for contract in self._contracts]
 
 
 class Contract:
-    pass
+    all = []
+
+    def __init__(self, author, book, date, royalties):
+        if not isinstance(date, str):
+            raise TypeError("Date must be a string")
+        if not isinstance(royalties, int):
+            raise TypeError("Royalties must be an integer")
+        self.author = author
+        self.book = book
+        self.date = date
+        self.royalties = royalties
+        author.contracts().append(self)
+        book.contracts().append(self)
+        Contract.all.append(self)
+
+    @staticmethod
+    def contracts_by_date(date):
+        if not isinstance(date, str):
+            raise TypeError("Date must be a string")
+
+        contracts_for_date = [contract for contract in Contract.all if contract.date == date]
+        return sorted(contracts_for_date, key=lambda x: datetime.strptime(x.date, '%m/%d/%Y'))
